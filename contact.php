14e276489d676d5246f6dc5f140e9f1fc5318742
123saunika <upretisaunika1234@gmail.com>
2024-05-13 07:20:02 +0545

    feat:init commit

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php include('partials-front/menu.php'); ?>
+
+    <!-- Contacts Section Starts Here -->
+    <section class="contact">
+        <div class="container">
+            <h3 class="text-center text-white">Contact Us Here</h3><br><br>
+            
+                <div class="text-center text-white">
+                    98123445566 <br>
+                    Saunika Upreti <br>
+                    CEO <br>
+                    K3 Bakery <br>
+                </div>
+            
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Contacts Section Ends Here -->
+
+
+    <?php include('partials-front/footer.php'); ?>
